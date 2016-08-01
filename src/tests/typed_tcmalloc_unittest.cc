96adcf358eeb39831c98af3cf175aa1c4c5b956d
Chris Ouwehand <chris.ouwehand@vu.nl>
2016-08-01 11:40:19 +0200

    Modified the typed test a bit.
    Added producer-consumer tests.

--- src/tests/typed_tcmalloc_unittest.cc
+++ src/tests/typed_tcmalloc_unittest.cc
@@ -1,59 +1,143 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <iostream>
+#include <pthread.h>
 #include <gperftools/typed_tcmalloc.h>
 #include "base/logging.h"       // for logging utilities
+#include "thread_cache.h"       // for ThreadCache
 #include "static_vars.h"        // for pageheap lookup
 #include "span.h"               // for span struct
 #include "common.h"             // for PageID
 
+using namespace std;
 using namespace tcmalloc;
 
-PageID malloc_free_pair_checks(const size_t size, const TypeTag type, PageID prev)
-{
-  void* ptr = tc_typed_malloc(size, type);
-  const PageID p = reinterpret_cast<uintptr_t>(ptr) >> kPageShift;
-  Span *span = Static::pageheap()->GetDescriptor(p);
-  size_t size_cl = Static::sizemap()->ByteSizeForClass(span->sizeclass);
+#define TRANSFER_LIMIT  (3 << 20) // 3MB
+#define ALLOCATION_SIZE (1 << 12) // 4KB
+#define DEFAULT_TYPE    123
 
-  CHECK_EQ(span->type, type);
+#define spanToSize(span) Static::sizemap()->ByteSizeForClass(span->sizeclass)
+#define ptrToSpan(ptr) Static::pageheap()->GetDescriptor( \
+                         reinterpret_cast<uintptr_t>(ptr) >> kPageShift)
 
-  if (prev != span->start) {
-    LOG(INFO, "new span: %p", span->start);
+struct Data {
+  void* ptr;
+  TypeTag type;
+  bool last;
+};
+
+pthread_t tid;
+pthread_mutex_t lock;
+pthread_cond_t consumed, produced;
+Data data;
+
+void *producer(void* args) {
+  size_t total = 0;
+
+  while (total < TRANSFER_LIMIT) {
+    pthread_mutex_lock(&lock);
+    while (data.ptr)
+      pthread_cond_wait(&consumed, &lock);
+
+    data.ptr = tc_typed_malloc(ALLOCATION_SIZE, DEFAULT_TYPE);
+    CHECK_NE(data.ptr, 0);
+    total += ALLOCATION_SIZE;
+
+    data.last = total >= TRANSFER_LIMIT;
+    data.type = DEFAULT_TYPE;
+
+    pthread_cond_signal(&produced);
+    pthread_mutex_unlock(&lock);
   }
-  // CHECK_EQ((uintptr_t)span->objects, (uintptr_t)ptr + size_cl);
+
+  pthread_exit(0);
+}
+
+void *consumer(void* args) {
+  bool done = false;
+  void *ptr = NULL;
+  TypeTag type = 0;
+
+  do {
+    pthread_mutex_lock(&lock);
+    while (!data.ptr)
+      pthread_cond_wait(&produced, &lock);
+
+    ptr = data.ptr;
+    type = data.type;
+    done = data.last;
+    data.ptr = NULL;
+
+    pthread_cond_signal(&consumed);
+    pthread_mutex_unlock(&lock);
+
+    CHECK_EQ(ptrToSpan(ptr)->type, type);
+
+    free(ptr);
+  } while (!done);
+
+  pthread_exit(0);
+}
+
+void test_producer_consumer() {
+  data.ptr = NULL;
+
+  CHECK_EQ(pthread_mutex_init(&lock, NULL), 0);
+  CHECK_EQ(pthread_cond_init(&produced, NULL), 0);
+  CHECK_EQ(pthread_cond_init(&consumed, NULL), 0);
+  CHECK_EQ(pthread_create(&tid, NULL, &producer, NULL), 0);
+
+  consumer(NULL);
+  pthread_join(tid, NULL);
+
+  pthread_cond_destroy(&produced);
+  pthread_cond_destroy(&consumed);
+  pthread_mutex_destroy(&lock);
+}
+
+void malloc_free_pair_checks(const size_t size, const TypeTag type)
+{
+  ThreadCache *heap = ThreadCache::GetCache();
+  size_t
+    cl = Static::sizemap()->SizeClass(size),
+    cl_size = Static::sizemap()->class_to_size(cl),
+    size_before = heap->Size();
+
+  void* ptr = tc_typed_malloc(size, type);
+  Span *span = ptrToSpan(ptr);
+
+  // Some checks
+  CHECK_EQ(span->type, type);
+  CHECK_GE(spanToSize(span), size);
 
   free(ptr);
-  // CHECK_EQ(span->objects, ptr);
+}
 
-  return span->start;
+void test_malloc_free_pairs() {
+  short skip;
+
+  for (TypeTag type = 64; type <= 1024; type += 64) {
+    for (int size = 0; size <= kMaxSize; size++) {
+      if      (UNLIKELY(size == 0))    skip = 0;
+      else if (UNLIKELY(size <= 1024)) skip = 7;
+      else                             skip = 127;
+
+      malloc_free_pair_checks(size, type);
+
+      if (LIKELY(size > 0)) {
+        size += skip;
+        malloc_free_pair_checks(size, type);
+      }
+    }
+  }
 }
 
 int main()
 {
-  short skip;
-  const TypeTag type = 123;
-  PageID pg = 0;
-
-  for (int i = 0; i <= kMaxSize; i++) {
-    if (UNLIKELY(i == 0)) {
-      skip = 0;
-    } else if (UNLIKELY(i <= 1024)) {
-      skip = 7;
-    } else {
-      skip = 127;
-    }
-    LOG(INFO, "Testing range: %d-%d", i, i+skip);
-
-    pg = malloc_free_pair_checks(i, type, pg);
-
-    if (LIKELY(i > 0)) {
-      i += skip;
-      pg = malloc_free_pair_checks(i, type, pg);
-    }
-  }
-
-  // LOG(INFO, "%d %d", kMaxSize, 13313);
-  // malloc_free_pair_checks(13313, 123);
+  LOG(INFO, "Running malloc-free pairs test.");
+  test_malloc_free_pairs();
+  LOG(INFO, "Running producer-consumer test.");
+  test_producer_consumer();
 
   printf("PASS\n");
   return 0;
