02973c313ba563c551bcd2a5018b408091902c5e
Taddeus Kroes <taddeuskroes@gmail.com>
2018-08-06 18:37:51 +0200

    Revert "Use epoll instead of poll for uffd polling"
    This reverts commit 0b01e0b385e8591a86e800ed0d72ac8bdc7402cf.
    
    Conflicts:
    	src/uffd-alloc.cc

--- src/uffd-alloc.cc
+++ src/uffd-alloc.cc
@@ -10,11 +10,11 @@
 #include <sys/syscall.h>        // for __NR_userfaultfd
 #include <sys/mman.h>           // for mmap, munmap, MADV_DONTNEED, etc
 #include <sys/ioctl.h>          // for ioctl
-#include <sys/epoll.h>          // for epoll_*
 #include <linux/userfaultfd.h>  // for UFFD*
 #include <pthread.h>            // for pthread_*
 #include <string.h>             // for strerror
 #include <errno.h>              // for errno
+#include <poll.h>               // for poll, POLLIN, etc
 #include <fcntl.h>              // for O_NONBLOCK
 #include "base/basictypes.h"    // for PREDICT_FALSE
 #include "system-alloc.h"       // for SysAllocator, MmapSysAllocator, mmap_space
@@ -106,22 +106,15 @@ static void *uffd_poller_thread(void*) {
 
   ldbg("uffd: start polling");
 
-  int epollfd = epoll_create1(O_CLOEXEC);
-  if (epollfd < 0)
-    lperror("epoll_create");
-
-  struct epoll_event event = { .events = EPOLLIN, .data = { .fd = uffd } };
-  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, uffd, &event) < 0)
-    lperror("epoll_ctl");
-
   for (;;) {
     // Wait for message. We don't use a timeout, the thread just ends when the
     // main program ends.
-    int nready = epoll_wait(epollfd, &event, 1, -1);
+    struct pollfd pollfd = { .fd = uffd, .events = POLLIN };
+    int nready = poll(&pollfd, 1, -1);
     if (nready < 0)
-      lperror("epoll_wait");
+      lperror("poll");
     ASSERT(nready == 1);
-    assert(event.events & EPOLLIN);
+    ASSERT(pollfd.revents & POLLIN);
 
     // Read message; We only expect page faults.
     struct uffd_msg msg;
